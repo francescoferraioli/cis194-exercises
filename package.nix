1f569b84699e401664fefa2adb555c1e3ef1d7b5
Brad Parker <hi@bradparker.com>
2018-08-29 12:15:30 +1000

    Week 03

--- package.nix
+++ package.nix
@@ -1,9 +1,9 @@
-{ mkDerivation, base, containers, hspec, mtl, stdenv }:
+{ mkDerivation, array, base, containers, hspec, mtl, stdenv }:
 mkDerivation {
   pname = "stacktrace-cis194";
   version = "0.1.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base containers mtl ];
+  libraryHaskellDepends = [ array base containers mtl ];
   testHaskellDepends = [ base containers hspec ];
   homepage = "https://github.com/stacktracehq/cosmos/tree/master/training/CIS194";
   description = "Resources for working through CIS194 together";
